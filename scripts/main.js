6563e26dbfdc3cc8362cbc7310a2ba8ecee2ebc3
liuchunzhao <snow330@qq.com>
2020-11-10 16:43:10 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc == 'images/firefox-icon.png'){
-        myImage.setAttribute('src', 'images/222.jpg');
+    if(mySrc == 'images/111.png'){
+        myImage.setAttribute('src', 'images/222.png');
     } else {
-        myImage.setAttribute('src', 'images/firefox-icon.png')
+        myImage.setAttribute('src', 'images/111.png')
     }
 }
 
